8f468f30ce58fdcf3eef0d5f1d3624eb1fc42733
Mayank Awasthi <mayankawasthi20@gmail.com>
2022-03-20 21:03:23 -0400

    Sixth commit

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -8,40 +8,35 @@ import { faSignIn } from '@fortawesome/free-solid-svg-icons'
 
 
 const LogIn = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async(event) => {
     event.preventDefault()
     // console.log('username: ', username);
     // console.log('password: ', password);
-    await fetch ("https://fathomless-ocean-09578.herokuapp.com/api/users/login", {
+    const response = await fetch ("https://fathomless-ocean-09578.herokuapp.com/api/users/login", {
       method: "POST",
       headers: {
         "Content-Type" : "Application/json",
       },
       body: JSON.stringify({
         username: username,
-        passowrd: password
+        password: password
       })
     }).then(response => response.json())
       .then(result => {
         console.log(result);
-
-        if (!result) {
-          alert("You may enter the wrong username or password")
-        }
-        else {
-          const token = result.token;
-          localStorage.setItem("userToken", token);
-          alert("You have been logged in!!!")
-        }
+        const token = result.token;
+        const user = result.user.id;
+        localStorage.setItem("userToken", token);
+        localStorage.setItem("userID", user);
       })
       .catch(console.error);
 
       
-    setUsername('');
-    setPassword('');
+    setUsername("");
+    setPassword("");
   }
 
   const handleChange = (event) => {
@@ -52,10 +47,10 @@ const LogIn = () => {
     <div id='container'>
       <form onSubmit={handleSubmit}>
         <label htmlFor='username'>Username:</label>
-        <input type='text' name='username' value={username} onChange={handleChange} />
+        <input type='text' className='username' value={username} onChange={handleChange} />
 
         <label htmlFor='password'>Password:</label>
-        <input type='password' name='password' value={password} onChange={(event) => setPassword(event.target.value)} />
+        <input type='password' className='password' value={password} onChange={(event) => setPassword(event.target.value)} />
         
 
         <button type='submit'>Log In</button>
